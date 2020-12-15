581a713ec73e3f4dfad583255c0e9e1158577439
nisc0055 <nisc0055@stud.hs-kl.de>
2020-12-15 13:30:32 +0100

    Added Main Menu

--- app/src/main/java/de/hskl/gatav/flappybender/views/MenuActivity.java
+++ app/src/main/java/de/hskl/gatav/flappybender/views/MenuActivity.java
@@ -2,49 +2,40 @@ package de.hskl.gatav.flappybender.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import de.hskl.gatav.flappybender.R;
 import de.hskl.gatav.flappybender.sound.Discman;
 
-public class PreActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
-    ImageView start;
+    Button start;
+    Button highscore;
+    Button options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         getSupportActionBar().hide();
+        Discman.getInstance().setSong(R.raw.main_menu);
 
-        setContentView(R.layout.activity_pre);
+        start = findViewById(R.id.BUTTON_PLAY);;
+        highscore = findViewById(R.id.BUTTON_HIGHSCORE);
+        options = findViewById(R.id.BUTTON_OPTIONS);
 
-        Intent intent = new Intent(PreActivity.this, Discman.class);
-        startService(intent);
+        start.setOnClickListener(this::startGame);
 
-
-        start = findViewById(R.id.START_APP);
-        ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(start, PropertyValuesHolder.ofFloat("scaleX", 1.2f), PropertyValuesHolder.ofFloat("scaleY", 1.2f), PropertyValuesHolder.ofFloat("alpha", 1f));
-        animator.setDuration(1000);
-
-        animator.setRepeatCount(ObjectAnimator.INFINITE);
-        animator.setRepeatMode(ObjectAnimator.REVERSE);
-        animator.start();
-
-        start.setOnClickListener(this::startApp);
     }
 
-    public void startApp(View v){
-        Intent intent = new Intent(PreActivity.this, MenuActivity.class);
+    public void startGame(View v){
+        Intent intent = new Intent(MenuActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
